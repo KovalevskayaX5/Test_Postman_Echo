af670879fc31dd960a331f9f792aed25665f0b1a
YanaK <Yana.Koval15@yandex.ru>
2023-05-08 13:46:38 +0300

    main

---
+++ src/test/java/Postman_EchoTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Postman_EchoTest {
+    @Test
+    public void shouldReturnBody() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("14/04/1991") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("14/04/1991"))
+        ;
+    }
+
+}
